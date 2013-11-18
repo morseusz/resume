b5653e0c3dd388c8178c832f56867568f2b4b943
piotr <morseusz@gmail.com>
2013-11-18 22:55:31 +0000

    Changed db backend to psql

--- resume/settings.py
+++ resume/settings.py
@@ -57,13 +57,20 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_resume',
+        'USER': 'django_resume',
+        'PASSWORD': 'django_resume',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+#Uncomment for heroku deployment
+# Parse database configuration from $DATABASE_URL
+#DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -77,10 +84,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
