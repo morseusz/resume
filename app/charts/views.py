cd737cdcc543a13e6ea1bbe027b1b0535b63926e
piotr <morseusz@gmail.com>
2013-11-21 01:07:53 +0000

    moving average view

--- app/charts/views.py
+++ app/charts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+from lib.statistics.average import moving_average
+from app.charts.models import get_model
+import lib.plot.time
 
-# Create your views here.
+
+def display_moving_average(request, alias):
+    #TODO: handle invalid requests types & missing args
+    model = get_model(alias)
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+    ma = request.GET.get('ma')
+
+    start, end = [datetime.strptime(x, '%Y-%m-%d') for x in start, end]
+    ma = ma.split(',') if ',' in ma else ma
+
+    data = model.objects.get_date_range(start, end)
+    canvas = lib.plot.time.moving_average(data, *ma)
+
+    response=HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    return response
+
