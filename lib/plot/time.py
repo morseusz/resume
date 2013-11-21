f9a6e838d76e292e04db3ac035ca8377b3f974f0
piotr <morseusz@gmail.com>
2013-11-21 01:08:52 +0000

    Fixed get_canvas and added specific function

--- lib/plot/time.py
+++ lib/plot/time.py
@@ -1,16 +1,24 @@
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib.figure import Figure
-from matplotlib.dates import DateFormatter
+import matplotlib.pyplot as plt
+import lib.statistics.average
 
 
-def get_canvas(*series):
-    figure = Figure()
-    x = figure.add_subplot(111)
+def get_canvas(series):
+    top = plt.subplot2grid((4,4), (0, 0), rowspan=3, colspan=4)
 
     for serie in series:
         dates, values = zip(*serie)
-        x.plot_date(dates, values, '-')
+        top.plot_date(dates, values, '-')
 
-    x.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
-    figure.autofmt_xdate()
-    return FigureCanvasAgg(figure)
+    plt.title('Microsoft Opening Stock Price from 2007 - 2012')
+    plt.legend()
+    return FigureCanvasAgg(plt.gcf())
+
+
+def moving_average(data, *ma):
+    data = [(x.date, float(x.average_price)) for x in data]
+    series = [data]
+    ma = [int(x) for x in ma]
+    for window_size in ma:
+        series.append(lib.statistics.average.moving_average(data, window_size))
+    return get_canvas(series)
